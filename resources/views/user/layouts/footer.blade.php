16e3ec83b913042b8c37b7022cfed06aa84ab542
phamphanbang <phamphanbang@gmail.com>
2022-12-05 23:06:03 +0700

    add nav and footer

---
+++ resources/views/user/layouts/footer.blade.php
@@ -0,0 +1,39 @@
+<div class="footer">
+    <div class="mt-4 d-flex flex-row w-100">
+        <div class="footer-logo d-flex flex-column w-20 ps-3">
+            <a class="navbar-brand" href="{{ route('home') }}">
+                <span class="logo-span-small p-0">TOEICAMP</span>
+            </a>
+            <small class="d-block mb-2">© 2022</small>
+            <div class="logo-block d-flex flex-row">
+                <a class="logo-social" href="#">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a class="logo-social" href="#">
+                    <i class="bi bi-twitter"></i>
+                </a>
+                <a class="logo-social" href="#">
+                    <i class="bi bi-linkedin"></i>
+                </a>
+                <a class="logo-social" href="#">
+                    <i class="bi bi-youtube"></i>
+                </a>
+            </div>
+        </div>
+        <div class="footer-info d-flex flex-row justify-content-end w-80">
+            <div class="footer-resource d-flex flex-column px-2">
+                <span class="resource-span">Tài nguyên</span>
+                <a class="resource-link" href="#">Full Test</a>
+                <a class="resource-link" href="#">Part Test</a>
+                <a class="resource-link" href="#">Mini Test</a>
+            </div>
+            <div class="footer-resource d-flex flex-column px-2 me-4">
+                <span class="resource-span">TOEICAMP</span>
+                <a class="resource-link" href="#">Về chúng tôi</a>
+                <a class="resource-link" href="#">Liên Hệ</a>
+                <a class="resource-link" href="#">Điều khoản bảo mật</a>
+                <a class="resource-link" href="#">Điều khoản sử dụng</a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/user/layouts/nav.blade.php
+++ resources/views/user/layouts/nav.blade.php
@@ -1,22 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="{{ route('home') }}">
+      <span class="logo-span-small">TOEICAMP</span>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Trang chủ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Đề thi online</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Blogs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="btn btn-primary login-button rounded-pill ms-2" href="#" role="button">Đăng nhập</a>
+          <!-- <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div> -->
         </li>
       </ul>
     </div>
